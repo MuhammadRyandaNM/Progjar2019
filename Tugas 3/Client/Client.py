f4dd39faf8456965ed68cbc8e76090d30d7dd1f5
MuhammadRyandaNM <muhammadryandanm@gmail.com>
2019-03-27 16:32:46 +0700

    Try

---
+++ Tugas 3/Client/Client.py
@@ -0,0 +1,113 @@
+
+
+import socket
+import os
+class Client:
+    def __init__(self):
+        socketbind = 9000
+        ip = "127.0.0.1"
+        self.server = (ip, socketbind)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(self.server)
+    
+    def close(self):
+        self.sock.send("#close")
+    
+    
+    def sendMessage(self):
+        while (True):
+            
+            self.sock.send("#askFiles")
+            datas = (self.sock.recv(1024))
+            datas = datas.strip()
+            datas = datas.replace(' ','')
+            datas = datas.replace('[','')
+            datas = datas.replace(']','')
+            datas = datas.replace('\'','')
+            datas = datas.split(',')
+            
+            ranged = []
+            for a in range(len(datas)):
+                datas[a]=datas[a].replace('\\\\','/')
+                print ("%s. %s"%(a, datas[a]))
+
+                ranged.append(a)   
+            print("select by its number")
+            fileask = raw_input()
+            fileask = int(fileask)
+            if not fileask in ranged:
+                continue
+            
+            first = True
+            fileask = str(fileask)
+            self.sock.sendall("#ask")
+            self.sock.send(fileask)
+            data = self.sock.recv(1024)
+
+            
+            if ("#sendingFile" in data):
+                data = self.sock.recv(1024)
+                if ("#start" in data):
+                    data= self.sock.recv(1024)
+                    filename = data.rstrip()
+                   
+                    filename = filename.replace("\\", "/")
+                    print ("[+] receiving " + str(filename))
+                    filename = filename[2:]
+                    fp = open(filename, "wb+")
+                    while True:
+                        data = self.sock.recv(1024)        
+                        
+                        if "##EOF" in data:
+                            print(data)
+                            print ("[-] received " + filename )
+                            fp.close()
+                            break
+                        fp.write(data)
+            else:
+                
+                while True:
+                    
+                    data = self.sock.recv(1024)
+                    
+                    if ("#directoryEnd" in data):
+                        print "[-] all files done"
+                        break
+                    
+                    elif ("#directoryName" in data):
+                        
+                        data = self.sock.recv(1024)
+                        data = data.strip()
+                        if(first):
+                            data = data.split('/')
+                            data = data[len(data)-1]
+                        print "[+] receiving directory" +data
+                        if (not os.path.exists(data)):
+                            os.mkdir(data)
+                        os.chdir(data)
+                        
+                    elif ("#directoryDone" in data):
+                        print "[-] current directory done"
+                        os.chdir('..')
+                        
+                    elif("#start" in data):
+                        data= self.sock.recv(1024)
+                        filename = data.strip()
+                        print ("[+] receiving " + str(filename))
+                        print filename
+                        fp = open(filename, "wb+")
+                        while True:
+                            data = self.sock.recv(1024)        
+                            
+                            if "##EOF" in data:
+                                fp.close()
+                                break
+                            fp.write(data)
+
+if __name__ == "__main__":
+    try:
+        main = Client()
+        main.sendMessage()
+    except KeyboardInterrupt:
+        main.close()
+        print("done")
---
+++ Tugas 3/README.md
@@ -0,0 +1,18 @@
+# Tugas 3 - Progjar E
+```
+Nama : Muhammad Ryanda Nugraha M
+NRP : 05111640000180
+```
+## Deskripsi Tugas
+
+![DeskripsiTugas](DeskripsiTugas.PNG)
+
+
+## Cara Penggunaan
+Berikut ini adalah instruksi tentang cara penggunaan script ini :
+
+1.Run Server.py
+2.Run Client.py
+3.Enjoy
+
+
